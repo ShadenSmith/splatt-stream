3a5aefb9053f0315c6584fb5447d4a9a4679ea58
Shaden Smith <shaden.smith@intel.com>
2019-06-03 16:26:53 -0700

    fixes format string missing definition

--- cmake/flags.cmake
+++ cmake/flags.cmake
@@ -2,6 +2,9 @@
 set(CMAKE_C_STANDARD   99)
 set(CMAKE_CXX_STANDARD 11)
 
+# Ensures we have the SPLATT_PF_* format strings defined.
+add_definitions(-D__STDC_FORMAT_MACROS=1)
+
 set(SPLATT_FLAGS "${SPLATT_FLAGS} -funroll-loops")
 set(SPLATT_FLAGS "${SPLATT_FLAGS} -fstrict-aliasing")
 set(SPLATT_FLAGS "${SPLATT_FLAGS} -fPIC")
